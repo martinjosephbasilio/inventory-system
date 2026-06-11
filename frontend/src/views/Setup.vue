8e4ab9bdd9206e1d5933bdb66027f45c7a07aac7
martinjosephbasilio <martinjosephbasilio@gmail.com>
2026-06-11 14:14:58 +0800

    Update

--- frontend/src/views/Setup.vue
+++ frontend/src/views/Setup.vue
@@ -199,7 +199,7 @@
     <span class="currency">₱</span>
     <input type="number" step="0.01" v-model.number="form.sell_pack" class="form-control" placeholder="0.00" />
   </div>
-  <small>Presyo kapag bumili ang customer ng isang pack</small>
+  
 </div>
               </div>
             </div>
